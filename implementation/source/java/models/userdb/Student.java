c01643d0166481f399455203a644ef6ee876b133
ylevitsk <ylevitsk@aol.com>
2014-04-13 16:21:36 -0700

    Update Student.java

---
+++ implementation/source/java/models/userdb/Student.java
@@ -0,0 +1,9 @@
+package userdb;
+@author Yuliya Levitskaya
+/**
+ * Class defines a student
+ * permission level of a student is set as 1
+ */
+public abstract class Student {
+	static int permission = 1;
+}
