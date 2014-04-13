bb58a468e4cfef7317b9f2a61107ccb7563edfcc
ylevitsk <ylevitsk@aol.com>
2014-04-13 16:14:58 -0700

    Update Settings.java

---
+++ implementation/source/java/models/userdb/Settings.java
@@ -0,0 +1,29 @@
+package userdb;
+
+@author Yuliya Levitskaya
+
+import java.util.Date;
+
+import testdb.Test;
+
+
+/**
+ * This class displays the settings for a particular test.
+ */
+public abstract class Settings {
+	String type;
+	Date start;
+	Date end;
+	String password;
+	String notes;
+	String gradingType;
+	Test test;
+    	/**
+     	* Method used in publishing the test.
+	* @param t - test to publish
+	* test must be already in thes userdb
+     	*/
+	abstract boolean publish(Test t){
+      System.out.println("In Settings.publish.");
+	}
+}
