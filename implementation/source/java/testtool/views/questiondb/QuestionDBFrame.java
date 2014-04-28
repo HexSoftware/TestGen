f27b1488b5b927edad910ecc24f681fa194f934a
nnordhof <nrnord219@gmail.com>
2014-04-28 09:37:31 -0700

    added removing questions

--- implementation/source/java/testtool/views/questiondb/QuestionDBFrame.java
+++ implementation/source/java/testtool/views/questiondb/QuestionDBFrame.java
@@ -8,15 +8,19 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
 import javax.swing.table.*;
 
 import testtool.models.questiondb.*;
 import testtool.views.commandmenu.CMBuilder;
-import testtool.views.testdb.GenerateTypeGUI;
+//import testtool.views.testdb.GenerateTypeGUI;
 
 /**
  * @author Neil Nordhof (nnordhof@calpoly.edu)
- * @version 21apr14
+ * @version 28apr14
  * 
  * The main view class for the Question Databank. 
  */
@@ -25,7 +29,10 @@ import testtool.views.testdb.GenerateTypeGUI;
 public class QuestionDBFrame {
 
 	private QuestionDatabank qdb;
-
+	
+	final private JFrame frame;
+	final private JTable table;
+	final private JButton editButton, removeButton, generateButton;
 	/**
 	 * Create the GUI and show it. For thread safety, this method should be
 	 * invoked from the event-dispatching thread.
@@ -33,7 +40,7 @@ public class QuestionDBFrame {
 	public QuestionDBFrame() {
 		qdb = new QuestionDatabank();
 		// Create and set up the window.
-		final JFrame frame = new JFrame("QuestionDB");
+		frame = new JFrame("QuestionDB");
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.setJMenuBar(CMBuilder.createMenuBar(new JMenuBar()));
 		frame.setLayout(new BorderLayout());
@@ -56,8 +63,10 @@ public class QuestionDBFrame {
 				return qdb.questions.get(row).question.get(col);
 			}
 		};
-		final JTable table = new JTable(dataModel);
+		dataModel.addTableModelListener(new DatabankDataListener());
+		table = new JTable(dataModel);
 		table.setPreferredSize(new Dimension(700, 400));
+		
 		table.getColumnModel().getColumn(0).setPreferredWidth(60);
 		table.getColumnModel().getColumn(1).setPreferredWidth(200);
 		table.getColumnModel().getColumn(2).setPreferredWidth(65);
@@ -70,16 +79,16 @@ public class QuestionDBFrame {
 		table.getColumnModel().getColumn(0).setHeaderValue(new String("Class"));
 		table.getColumnModel().getColumn(1).setHeaderValue(new String("Topic"));
 		table.getColumnModel().getColumn(2).setHeaderValue(new String("Type"));
-		table.getColumnModel().getColumn(3)
-				.setHeaderValue(new String("Question Text"));
-		table.getColumnModel().getColumn(4)
-				.setHeaderValue(new String("Difficulty"));
+		table.getColumnModel().getColumn(3).setHeaderValue(new String("Question Text"));
+		table.getColumnModel().getColumn(4).setHeaderValue(new String("Difficulty"));
 		table.getColumnModel().getColumn(5).setHeaderValue(new String("Time"));
-		table.getColumnModel().getColumn(6)
-				.setHeaderValue(new String("Last Used"));
-		table.getColumnModel().getColumn(7)
-				.setHeaderValue(new String("Author"));
-
+		table.getColumnModel().getColumn(6).setHeaderValue(new String("Last"));
+		table.getColumnModel().getColumn(7).setHeaderValue(new String("Author"));
+		
+		DefaultListSelectionModel selector = new DefaultListSelectionModel();
+		selector.addListSelectionListener(new DatabankListener());
+		table.setSelectionModel(selector);
+		
 		JScrollPane scrollpane = new JScrollPane(table);
 		scrollpane.setPreferredSize(new Dimension(750, 400));
 		qdbpanel.add(scrollpane);
@@ -102,27 +111,35 @@ public class QuestionDBFrame {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				new AddQuestion(qdb);
-				dataModel.fireTableDataChanged();
+				//dataModel.fireTableDataChanged();
+				//table.revalidate();
+				//TODO: find a way to listen for add question frame to be finished.
 			}
 		});
 		buttonPanel.add(addButton);
 		
-		JButton editButton = new JButton("Edit");
+		editButton = new JButton("Edit");
 		editButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				//new EditQuestion(qdb, table.getSelectedRow());
 				qdb.edit(null);
 			}
 		});
+		editButton.setEnabled(false);
 		buttonPanel.add(editButton);
 		
-		JButton removeButton = new JButton("Remove");
+		removeButton = new JButton("Remove");
 		removeButton.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				new RemoveFrame(qdb);
+			public void actionPerformed(ActionEvent e) {
+				int[] i = table.getSelectedRows();
+				new RemoveFrame(qdb, i);
+				System.out.println("136");
+				//dataModel.fireTableRowsDeleted(i[0], i[i.length-1]);
 			}
 		});
+		removeButton.setEnabled(false);
 		buttonPanel.add(removeButton);
 		
 		JButton filterButton = new JButton("Filter");
@@ -134,13 +151,16 @@ public class QuestionDBFrame {
 		});
 		buttonPanel.add(filterButton);
 		
-		JButton generateButton = new JButton("Generate");
+		generateButton = new JButton("Generate");
 		generateButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				new GenerateTypeGUI();
+				//new GenerateTypeGUI();
+				//TODO: Send selected questions to test generator.
+				System.out.println("Generate Pressed");
 			}
 		});
+		generateButton.setEnabled(false);
 		buttonPanel.add(generateButton);
 
 		frame.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
@@ -150,5 +170,47 @@ public class QuestionDBFrame {
 		// Display the window.
 		frame.pack();
 		frame.setVisible(true);
+	}
+
+	private class DatabankDataListener implements TableModelListener {
+		@Override
+		public void tableChanged(TableModelEvent e) {
+			int frow = e.getFirstRow();
+			int lrow = e.getLastRow();
+			AbstractTableModel model = (AbstractTableModel)e.getSource();
+			if (e.getType() == TableModelEvent.INSERT) {
+				model.fireTableRowsInserted(frow, lrow);
+			}
+			else if (e.getType() == TableModelEvent.DELETE) {
+				model.fireTableRowsDeleted(frow, lrow);
+			}
+			else {
+				model.fireTableRowsUpdated(frow, lrow);
+			}
+			System.out.println("Listen!");
+		}
+	}
+	
+	
+	private class DatabankListener implements ListSelectionListener {
+		@Override
+		public void valueChanged(ListSelectionEvent e) {
+			if (table.getSelectedRowCount() == 1) {
+				removeButton.setEnabled(true);
+				editButton.setEnabled(true);
+				generateButton.setEnabled(true);
+			}
+			else if (table.getSelectedRowCount() > 1) {
+				removeButton.setEnabled(true);
+				editButton.setEnabled(false);
+				generateButton.setEnabled(true);
+			}
+			else {
+				removeButton.setEnabled(false);
+				editButton.setEnabled(false);
+				generateButton.setEnabled(false);
+			}
+			table.revalidate();
+		}
 	}
 }
