fa7ede0beb53f59a4d0b2975e6ac462ad5aed557
ylevitsk <ylevitsk@aol.com>
2014-04-28 19:39:46 -0700

    update 7:38pm

---
+++ testing/implementation/source/java/testtool/models/userdb/ProctorTEST.java
@@ -0,0 +1,24 @@
+package testtool.models.userdb;
+
+/****
+ *
+ * Class ProctorTEST is the companion testing class for Proctor.
+ *  It implements the following module
+ * test plan:
+ *									  <pre>
+ *    Phase 1: Unit test the constructor.
+ *
+ *    Phase 2: Unit test listOfTests method
+ *
+ *    Phase 3: Unit test grade method.
+ *
+ *    Phase 4: Unit test options method.
+ *
+ *    Phase 5: Stress test by constructing and destructing items collection of 
+ *             100000 elements.
+ *									 </pre>
+ */
+public class ProctorTEST extends Proctor {
+
+
+}
