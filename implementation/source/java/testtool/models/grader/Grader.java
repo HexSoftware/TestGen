abd39359373d7103bf6d96a0398a6c5aedf172bb
ylevitsk <ylevitsk@aol.com>
2014-04-28 20:15:26 -0700

    update 8:05pm

--- implementation/source/java/testtool/models/grader/Grader.java
+++ implementation/source/java/testtool/models/grader/Grader.java
@@ -2,31 +2,102 @@ package testtool.models.grader;
 import testtool.models.testdb.Test;
 import testtool.models.userdb.Student;
 
+/**
+ * @author Kevin Pham (kpham11@calpoly.edu)
+ * @version 26apr14
+ * 
+ * Class defines a grader
+ */
+
 
 public class Grader {
-	int permissionLevel;
+	static int permission = 3;
 	
+	//Constructs a grader 
 	public Grader() {
 		System.out.println("In grader.Constructor.");
 	}	
 	
-    public void releaseTest(Test test, Grader options) {
-		System.out.println("In grader.releaseTest.");
+	
+	/**
+	 * Releases a test with specified options
+	 * @param test - the test the grader wants to release
+	 * @param options - the options the grader wants to release the test with
+	 */
+	/*@
+	requires
+		(*
+		 * The test is ready for release
+		 * There's atleast one release option
+		 *);
+	ensures
+		(*
+		 * Students will be able to see their test results/
+		 *);
+ */
+    public void releaseTest(Test test, ReleaseOptions options) {
+		System.out.println("In graer.releaseTest.");
 	}
 	
-	public void grade(Test test) {
-		System.out.println("In grader.grade.");
-	}
-	
+
+    /**
+	 * Releases a test with specified options
+	 * @param test - the test the grader wants to release
+	 * @param options - the options the grader wants to release the test with
+	 */
+    /*@
+	requires
+		(*
+		 * The student exists
+		 *);
+	ensures
+		(*
+		 * Student will be selected
+		 *);
+ */
 	public void selectStudent(Student student){
 		System.out.println("In grader.selectStudent.");
 	}
 	
+	
+	
+	/**
+	 * Saves the progress of grading of a test
+	 * @param test - the test the grader wants to save
+	 * 
+	 */
+	/*@
+	requires
+		(*
+		 * The Test hasn't already been released
+		 *);
+	ensures
+		(*
+		 * Progress of grading will be saved onto computer
+		 *);
+ */
 	public void saveProgress(Test test) {
 		System.out.println("In grader.saveProgress.");
 	}
 	
+	
+	/**
+	 * Finalizes grading for a test by setting flag for test completion to true
+	 * @param test - the test the grader wants to finalize
+	 * 
+	 */
+	/*@
+	requires
+		(*
+		 * Atleast one option has been selected
+		 *);
+	ensures
+		(*
+		 * Test will be given list of selected options
+		 *);
+ */
 	public void finishGrading(Test test) {
 		System.out.println("In grader.finishGrading.");
 	}
 }
+
