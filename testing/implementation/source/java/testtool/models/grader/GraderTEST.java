ef9b092cc92870bec95c2a34d40f4ac4bf512474
ylevitsk <ylevitsk@aol.com>
2014-04-28 20:05:54 -0700

    update 8:05pm

---
+++ testing/implementation/source/java/testtool/models/grader/GraderTEST.java
@@ -0,0 +1,17 @@
+/****
+ *
+ * Class GraderTest is the companion testing class for class <a href=
+ * Grader.html> Grader </a>.  It implements the following module test plan:
+ *                                                                         <ul>
+ *                                                                      <p><li>
+ *     Phase 1: Unit test the constructor.
+ *                                                                      <p><li>
+ *     Phase 2: Unit test the selectStudent and releaseTest methods
+ *                                                                      <p><li>
+ *     Phase 3: Unit test the saveProgress and finishGrading methods
+ *                                                                      <p><li>
+ *     Phase 4: Repeat phases 1 through 5.
+ *                                                                      <p><li>
+ *     Phase 5: Stress test by releasing 1000 tests.
+ *                                                                        </ul>
+ */
