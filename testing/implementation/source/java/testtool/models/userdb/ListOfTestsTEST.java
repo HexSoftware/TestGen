1f7d95f7c3b08ab7c2159eebe3a7612a3d78bce5
ylevitsk <ylevitsk@aol.com>
2014-04-28 19:39:46 -0700

    update 7:38pm

---
+++ testing/implementation/source/java/testtool/models/userdb/ListOfTestsTEST.java
@@ -0,0 +1,22 @@
+package testtool.models.userdb;
+
+/****
+ *
+ * Class ListOfTestsTEST is the companion testing class for
+ * ListOfTests.  It implements the following module
+ * test plan:
+ *									  <pre>
+ *    Phase 1: Unit test the constructor.
+ *
+ *    Phase 2: Unit test open, close methods
+ *          
+ *    Phase 3: Unit test grade.
+ * 
+ *    Phase 4: Stress test by constructing and destructing items collection of 
+ *             100000 elements.
+ *									 </pre>
+ */
+public class ListOfTestsTEST extends ListofTests {
+
+
+}
