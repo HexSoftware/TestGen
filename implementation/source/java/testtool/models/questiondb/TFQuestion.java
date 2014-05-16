14ac820a843b05948d085e6562147e8ba3061fd8
Robert Almada <RAlmada@users.noreply.github.com>
2014-05-16 10:27:02 -0700

    Update TFQuestion.java

--- implementation/source/java/testtool/models/questiondb/TFQuestion.java
+++ implementation/source/java/testtool/models/questiondb/TFQuestion.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
  * the student will simply specify whether or not a statement was true
  *
  * @author RJ Almada (rjalmada@calpoly.edu), Neil Nordhof (nnordhof@calpoly.edu)
- * @version 13may14
+ * @version 16may14
  *
  */
 public class TFQuestion extends Question {
