81962bb80ffa8df26110c4b7cde7a98cf3fc1386
Grant Pickett <grantrpickett@gmail.com>
2014-05-21 22:25:34 -0700

    Grant's tests and a couple updates

--- implementation/source/java/testtool/models/testdb/Test.java
+++ implementation/source/java/testtool/models/testdb/Test.java
@@ -3,9 +3,10 @@
  * It has a list of questions and other needed data
  * @author Grant Pickett, Yuliya Levitskaya
  * @version 5/13/2014
- */ 
+ */
 
 package testtool.models.testdb;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map.Entry;
@@ -13,59 +14,66 @@ import java.util.Map.Entry;
 import testtool.models.questiondb.Question;
 
 public class Test {
-   /*
-    * public String uniqueId; public String state; public Collection<Question>
-    * questions; public String testTitle; public String author; public String
-    * lastUsed; public String totalQuestions; public String totalPoints; public
-    * String totalTime; public String avgDifficulty; public String course;
-    */
-   HashMap<String, String> testParams   = new HashMap<String, String>();
-   ArrayList<Question>     questionList = new ArrayList<Question>();
+	/*
+	 * public String uniqueId; public String state; public Collection<Question>
+	 * questions; public String testTitle; public String author; public String
+	 * lastUsed; public String totalQuestions; public String totalPoints; public
+	 * String totalTime; public String avgDifficulty; public String course;
+	 */
+	public HashMap<String, String> testParams = new HashMap<String, String>();
+	public ArrayList<Question> questionList = new ArrayList<Question>();
 
-   public Test(HashMap<String, String> testP,
-         ArrayList<Question> questionL) {
-      testParams = testP;
-      questionList = questionL;
-   }
-  public Test(){
-	   
-   }
-   /*
-    * getTestParam returns the data in a given column, represented by the string    
-    * paramerer of this method
-    */
-   public String getTestParam(String column) {
-      return testParams.get(column);
-    }
-   /*
-    * setTestParam changes the data in a given column, represented by the first string   
-    * paramerer of this method and the second string parameter is the data
-    */
-   public String setTestParam(String column, String data) {
-	      return testParams.put(column, data);
-   }
-   /*
-   * getQuestionList returns the questions of this test
-   */
-   public ArrayList<Question> getQuestionList() {
-      return questionList;
-   }
-  public String toString(){
+	public Test(HashMap<String, String> testP, ArrayList<Question> questionL) {
+		testParams = testP;
+		if (testParams.get("password") == null) {
+			testParams.put("password", null);
+		}
+		questionList = questionL;
+	}
 
-    StringBuilder sb = new StringBuilder();
+	public Test() {
 
-    sb.append("begintest \n");
-    for (Entry<String, String> entry : testParams.entrySet()) {
-      sb.append("key: " + entry.getKey() + " value: "
-       + entry.getValue()); 
-      sb.append(" \n"); 
-      System.out.println(sb.toString());
-    }
-    for (Question entry : questionList){
-      sb.append(entry.toString()); 
-      sb.append(" \n"); 
-    }
-    sb.append("endtest \n");
-	return sb.toString();
-  }
+	}
+
+	/*
+	 * getTestParam returns the data in a given column, represented by the
+	 * string paramerer of this method
+	 */
+	public String getTestParam(String column) {
+		return testParams.get(column);
+	}
+
+	/*
+	 * setTestParam changes the data in a given column, represented by the first
+	 * string paramerer of this method and the second string parameter is the
+	 * data
+	 */
+	public String setTestParam(String column, String data) {
+		return testParams.put(column, data);
+	}
+
+	/*
+	 * getQuestionList returns the questions of this test
+	 */
+	public ArrayList<Question> getQuestionList() {
+		return questionList;
+	}
+
+	public String toString() {
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("begintest \n");
+		for (Entry<String, String> entry : testParams.entrySet()) {
+			sb.append("key: " + entry.getKey() + " value: " + entry.getValue());
+			sb.append(" \n");
+			System.out.println(sb.toString());
+		}
+		for (Question entry : questionList) {
+			sb.append(entry.toString());
+			sb.append(" \n");
+		}
+		sb.append("endtest \n");
+		return sb.toString();
+	}
 }
