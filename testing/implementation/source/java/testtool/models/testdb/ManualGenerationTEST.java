cae40ba13f8aa8f24e6a7216cef5ba93550def11
Grant Pickett <grantrpickett@gmail.com>
2014-05-21 22:26:35 -0700

    comment

--- testing/implementation/source/java/testtool/models/testdb/ManualGenerationTEST.java
+++ testing/implementation/source/java/testtool/models/testdb/ManualGenerationTEST.java
@@ -1,20 +1,5 @@
-/****
- *
- * Class ManualGenerationis the class that generates tests based on parameters. 
- * It implements the following module test plan:
- *                                                                         <ul>
- *                                                                      <p><li>
- *     Phase 1: Unit test the constructor.
- *                                                                      <p><li>
- *     Phase 2: Unit test the simple access method.
- *                                                                      <p><li>
- *     Phase 3: Unit test the complex methods Generation algorithm.
- *                                                                      <p><li>
- *     Phase 4: Repeat phases 1 through 3.
- *                                                                      <p><li>
- *     Phase 5: Stress test by adding, removing, editing, and deleting 100000 items.
- *                                                                        </ul>
- */
+
+
 package test;
 import org.junit.Test;
 
